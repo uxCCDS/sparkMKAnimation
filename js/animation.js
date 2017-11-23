f8eb6d874a78620f407687a6148289361a2501da
Arthus <531151995@qq.com>
2017-11-23 12:31:40 +0800

    add

---
+++ js/animation.js
@@ -0,0 +1,48 @@
+(function(){
+
+	var Arrow = function(){
+		this.Line = document.getElementById('svg_line');
+		this.LineTop = document.getElementById('svg_line_top');
+		this.LineBottom = document.getElementById('svg_line_bottom');
+		this.init();
+	};
+	Arrow.prototype={
+		init:function(){
+			this.initAsh();
+
+			this.AshGo.play();
+		},
+		initAsh:function(){
+			var me = this,
+				t1=50,
+				t2=30,
+				t3=30;
+			
+			this.AshGo = new Ash.S([{
+				dom:me.LineTop,
+				attr:[{'stroke-dashoffset':-140},{'stroke-dashoffset':0}],
+				time:t3,
+				tween:'easeInOutInt',
+				delay:t1+t2
+			},{
+				dom:me.LineBottom,
+				attr:[{'stroke-dashoffset':-90},{'stroke-dashoffset':0}],
+				time:t2,
+				tween:'easeOutInt',
+				delay:t1
+			},{
+				dom:me.Line,
+				attr:[{'stroke-dashoffset':-700},{'stroke-dashoffset':0}],
+				time:t1,
+				tween:'easeOutInt'
+			}],1,function(){
+
+			});
+		}
+	};
+
+	$(function(){
+		new Arrow();
+	});
+
+})();
